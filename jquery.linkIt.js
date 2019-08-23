5eef02805e9ad94a565ee7f113907715243a584f
murciecoiming2u <murcielago13311@hotmail.com>
2019-08-23 19:53:46 -0400

    Release version  0.20

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,1 +1,32 @@
-console.log("Hello");
+/*
+*	Name: linkIt
+*	Author: xwili
+*   Version: 0.1.0
+* 	License: MIT
+*/
+
+(function($){
+	$.fn.linkIt =  function(options){
+		// Default settings
+		var settings = $.extend({
+				href: null,
+				text: null,
+				target: '_self'
+		}, options);
+
+		// Validate
+		if(settings.href == null){
+			console.log("Please type an href for your link");
+			return this;
+		}
+
+		return this.each(function(){
+			var object = $(this);
+			if(settings.text == null){
+				settings.text = object.text();
+			}
+			object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+		});
+
+	}
+}(jQuery));
